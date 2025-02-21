84f76daba46f4c26cf9080c83b7e0fe001f53cd6
Vadim-Yavdoshliar <113513487+Vadim-Yavdoshliar@users.noreply.github.com>
2025-02-21 10:36:42 +0200

    Filter design accomplished.
    Doesn't imply any functional filtering yet.

---
+++ storeScript.js
@@ -0,0 +1,16 @@
+
+function onSelector(selectorEl){
+
+    if(selectorEl.style.backgroundColor === "rgb(220, 255, 103)"){
+        selectorEl.style.backgroundColor = "rgb(5, 5, 5)";
+        selectorEl.style.color = "rgb(209,209,209";
+        selectorEl.style.fontWeight = 'normal';
+    }
+    else {
+        selectorEl.style.backgroundColor = "rgb(220, 255, 103)";
+        selectorEl.style.color = "rgb(0,0,0";
+        selectorEl.style.fontWeight = 'bold';
+    }
+
+}
+
