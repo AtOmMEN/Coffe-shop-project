a024c64eed2faa3b91c84234a6947e373bf80797
Vadim-Yavdoshliar <vadik1610@gmail.com>
2025-03-25 13:54:51 +0200

    ready product panels(only visual). Must be tested on a laptop to confirm correspondence with original design

--- storeScript.js
+++ storeScript.js
@@ -3,12 +3,12 @@ function onSelector(selectorEl){
 
     if(selectorEl.style.backgroundColor === "rgb(220, 255, 103)"){
         selectorEl.style.backgroundColor = "rgb(5, 5, 5)";
-        selectorEl.style.color = "rgb(209,209,209";
+        selectorEl.style.color = "rgb(169,169,169)";
         selectorEl.style.fontWeight = 'normal';
     }
     else {
         selectorEl.style.backgroundColor = "rgb(220, 255, 103)";
-        selectorEl.style.color = "rgb(0,0,0";
+        selectorEl.style.color = "rgb(0,0,0)";
         selectorEl.style.fontWeight = 'bold';
     }
 
